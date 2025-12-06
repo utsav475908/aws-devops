a00dc296c68dc1c9afc75d3f8292c58a3e1cdf3a
Amitabh-DevOps <amitabhdevops2024@gmail.com>
2025-12-06 17:18:35 +0530

    Fix Terraform ECS deployment_circuit_breaker nesting

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -157,14 +157,12 @@ resource "aws_ecs_service" "app" {
     container_port   = var.container_port
   }
 
-  deployment_configuration {
-    maximum_percent         = 200
-    minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 100
 
-    deployment_circuit_breaker {
-      enable   = true
-      rollback = true
-    }
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
   }
 
   tags = {
